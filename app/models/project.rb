1ac0879787cd8cfe56818a1df50cc9945f5f7bf2
kynv-0324 <nguyen.van.ky@sun-asterisk.com>
2023-09-11 10:25:15 +0700

    Task 67441: Add resource

--- app/models/project.rb
+++ app/models/project.rb
@@ -26,6 +26,7 @@ class Project < ApplicationRecord
   has_many :project_features, dependent: :destroy
   has_many :project_health_items, dependent: :destroy
   has_many :health_items, through: :project_health_items
+  has_many :project_user_resources, through: :project_users
 
   delegate :name, to: :group, prefix: true
   enum status: {new: 0, in_progress: 1, maintaining: 2, pending: 3, close: 4},
@@ -50,10 +51,14 @@ class Project < ApplicationRecord
   scope :filter_date, ->(date){filter_by_date(date) if date.present?}
   scope :filter_status, ->(status){where status: status if status.present?}
   scope :filter_group, ->(group){where group_id: group if group.present?}
+
+  def sum_man_month
+    project_user_resources.sum(:man_month)
+  end
+
   class << self
     def filter_by_date date_str
-      date = valid_date date_str
-      date ? where(start_date: date) : all
+      Project.where("start_date LIKE ?", "#{date_str}%")
     end
   end
 end
